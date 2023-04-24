2116c107e9439da681b61677cbfeefeb68c00140
Ian Owira <ian@owira.co.za>
2023-04-24 18:25:01 +0200

    - return of the download button - ga4

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -10,10 +10,20 @@ window.addEventListener('load', function () {
   document.getElementById('download-cv').addEventListener('click', (evt) => {
     evt.preventDefault();
 
-    html2pdf(document.getElementById('home'), {
+    const homeEl = document.getElementById('home')
+
+    homeEl.classList.add("printing");
+
+    html2pdf(homeEl, {
       filename:     'ian-owira-cv.pdf',
+      enableLinks: true,
       jsPDF:        {  format: 'A4' }
     });
+
+    setTimeout(function() {
+      homeEl.classList.remove("printing");
+    }, 1000);
+
   });
 
 })
